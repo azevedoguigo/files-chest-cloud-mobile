047400633c088a5c889b21cb992853e5bc55dd18
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-10-21 08:50:00 -0300

    feat: signIn page.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 import NavigationBar from '../components/NavigationBar'
+import SignIn from './signIn'
 
 export default function Layout() {
   return(
     <View className='bg-zinc-900 h-screen w-screen justify-between'>
-      <View>
-        <Text>Olá, mundo!</Text>
-      </View>
+      <SignIn />
       <NavigationBar />
       <StatusBar style='auto'/>
     </View>
